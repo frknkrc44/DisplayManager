db6d6c1e788ce82ed7ae7ae9b46d2eb7a4d45639
frknkrc44 <krc440002@gmail.com>
2025-08-09 14:53:56 +0300

    perf: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "org.blinksd.dispmgr"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
